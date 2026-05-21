aa55811c500279921aec59ebbbe6e65d70e3cfda
Vladimir Erokhin <147151317+WarriorVortex@users.noreply.github.com>
2026-05-21 18:28:25 +0300

    Project setup (#1)
    * chore: initial project setup
    
    * feat: create and configure routes with blank pages

---
+++ src/app/app.config.ts
@@ -0,0 +1,26 @@
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import { routes } from './app.routes';
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withHashLocation()),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideNzI18n(en_US),
+    provideNzIcons(icons)
+  ]
+};
